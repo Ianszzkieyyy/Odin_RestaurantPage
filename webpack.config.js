044f902356fda32053e1589da0e38e38770b0958
Ian <87252958+Ianszzkieyyy@users.noreply.github.com>
2023-07-27 15:18:03 +0800

    finish webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        assetModuelFilename: '[name][ext]'
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|svg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Filipino Cuisine',
+            filename: 'index.html',
+            template: './src/template.html',
+        }),
+    ],
+}
